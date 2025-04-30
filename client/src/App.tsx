178301292308a8593d81356478036b2592268842
Mauriz66 <76722408+Mauriz66@users.noreply.github.com>
2025-04-30 18:44:51 -0300

    fix: simplify App.tsx to use only existing components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,7 @@
 import { Switch, Route, useLocation } from "wouter";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import NotFound from "@/pages/not-found";
-import Home from "@/pages/Home";
-import PlanoConexao from "@/pages/PlanoConexao";
-import BaralhoCigano from "@/pages/BaralhoCigano";
-import Leituras from "@/pages/Leituras";
-import Terapias from "@/pages/Terapias";
-import Sobre from "@/pages/Sobre";
-import Contato from "@/pages/Contato";
-import { Navbar } from "@/components/layout/Navbar";
-import { Footer } from "@/components/layout/Footer";
-import { WhatsAppButton } from "@/components/layout/WhatsAppButton";
 import { useEffect } from "react";
+import Home from "./pages/Home";
+import BaralhoCigano from "./pages/BaralhoCigano";
 
 function Router() {
   const [location] = useLocation();
@@ -22,33 +11,18 @@ function Router() {
   }, [location]);
 
   return (
-    <>
-      <Navbar />
-      <div className="pt-16 min-h-screen">
-        <Switch>
-          <Route path="/" component={Home} />
-          <Route path="/plano-conexao" component={PlanoConexao} />
-          <Route path="/baralho-cigano" component={BaralhoCigano} />
-          <Route path="/leituras" component={Leituras} />
-          <Route path="/terapias" component={Terapias} />
-          <Route path="/sobre" component={Sobre} />
-          <Route path="/contato" component={Contato} />
-          <Route component={NotFound} />
-        </Switch>
-      </div>
-      <Footer />
-      <WhatsAppButton />
-    </>
+    <div className="min-h-screen">
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/baralho-cigano" component={BaralhoCigano} />
+        <Route>Página não encontrada</Route>
+      </Switch>
+    </div>
   );
 }
 
 function App() {
-  return (
-    <TooltipProvider>
-      <Router />
-      <Toaster />
-    </TooltipProvider>
-  );
+  return <Router />;
 }
 
 export default App;
